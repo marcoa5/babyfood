0db9b157b9fbe675cbf425e0d83bb7facb8bb2dc
marcoa5 <58443501+marcoa5@users.noreply.github.com>
2020-08-27 12:14:06 +0200

    uikit+material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,17 +11,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatNativeDateModule } from '@angular/material/core';
+import { MatNativeDateModule, MatOption } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    UpbarComponent
+    UpbarComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,9 +32,9 @@ import {MatButtonModule} from '@angular/material/button';
     MatToolbarModule,
     MatDatepickerModule,
     MatFormFieldModule,
-    MatNativeDateModule,
-    MatInputModule,
-    MatIconModule, MatButtonModule,
+    MatNativeDateModule, MatSelectModule,
+    MatInputModule, MatSidenavModule,
+    MatIconModule, MatButtonModule, MatOptionModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     BrowserAnimationsModule
   ],
--- src/app/upbar/upbar.component.ts
+++ src/app/upbar/upbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upbar.component.css']
 })
 export class UpbarComponent implements OnInit {
-
+  showFiller = false;
   constructor() { }
 
   ngOnInit(): void {
